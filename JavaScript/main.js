6b5a304a85a551605501ebd5a7b47778763f8936
pertuzSalud <sebastianpertuzg@hotmail.com>
2022-06-08 11:31:15 -0500

    primer commit

---
+++ JavaScript/main.js
@@ -0,0 +1,93 @@
+const titulo = document.getElementById('h1');
+const input = document.querySelector('input');
+const info = document.getElementById('contenedor'); 
+const button = document.querySelector('button');
+const botoncito = document.getElementById('botoncito');
+const actualizar = document.getElementById('actualizar');
+const input2 = document.getElementById('input2');
+const input3 = document.getElementsById('input3');
+
+button.addEventListener('click', (e) =>{
+    e.preventDefault(); //Evita recargar la página
+    traerProductos(input.value);
+});
+
+botoncito.addEventListener('click', (s) => {
+    s.preventDefault();
+    registrarProductos();
+})
+
+input2-addEventListener('click', (actual) => {
+    actual.preventDefault();
+    actualProductos(input2.value);
+})
+
+
+
+function traerProductos(res){
+    fetch(`https://fakestoreapi.com/products/${res}`)
+    .then(res => res.json())
+    .then(data => {
+        verProductos(data)
+    })
+}
+
+function verProductos(nomProd){
+    //console.log(nomProd);
+    const nom = document.getElementById('nombreProducto');
+    nom.textContent = nomProd.title;
+
+    const img = document.getElementById('imgProducto');
+    img.src = nomProd.image;
+
+    const desc = document.getElementById('descripcion');
+    desc.textContent = nomProd.description;
+
+    const precio = document.getElementById('precio');
+    precio.textContent = nomProd.price;
+}
+
+function registrarProductos(){
+
+    fetch('https://fakestoreapi.com/products',{
+            method:"POST",
+            body:JSON.stringify(
+                {
+                    title: 'test product',
+                    price: 13.5,
+                    description: 'lorem ipsum set',
+                    image: 'https://i.pravatar.cc',
+                    category: 'electronic'
+                }
+            )
+        })
+            .then(res=>res.json())
+            .then(json=>console.log(json))
+}
+
+function actualProductos(num){
+
+    fetch(`https://fakestoreapi.com/products/${num}`,{
+        method:"PUT",
+        body:JSON.stringify(
+            {
+                title: 'Halo 5',
+                price: 86,
+                description: 'El último juego de una de las mejores sagas de todas',
+                image: 'https://i.pravatar.cc',
+                category: 'Games'
+            }
+        )
+    })
+
+    .then(res => res.json())
+    .then(json => console.log(json))
+}
+
+function eliminarProducto(id){
+    fetch(`https://fakestoreapi.com/products/${id}`,{
+            method:"DELETE"
+        })
+            .then(res=>res.json())
+            .then(json=>console.log(json))
+}
